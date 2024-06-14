564d7c80192ae36f809a5c93032a137db88b1877
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2024-06-14 16:13:11 +0800

    services

--- app/page.js
+++ app/page.js
@@ -5,31 +5,13 @@ import Services from '../components/pages/Services';
 import VideoBackground from '../components/VideoBackground';
 
 export default function Home() {
-  const [showSecondPage, setShowSecondPage] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      if (scrollY > 500) {
-        setShowSecondPage(true);
-      } else {
-        setShowSecondPage(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <div>
       <div className="relative min-h-screen">
         <VideoBackground />
       </div>
       <div className="min-h-screen bg-white flex items-center justify-center">
-        {showSecondPage && <Services />}
+        <Services />
       </div>
     </div>
   );
--- components/pages/Services.js
+++ components/pages/Services.js
@@ -1,20 +1,23 @@
-'use client';
+// pages/index.js
 import Image from 'next/image';
 import BackgroundImage from '../../assets/light-brown.jpg';
 import styles from '../../styles/VideoBackground.module.css';
-export default function Home() {
+
+export default function Services() {
   return (
     <div className={styles.imageBackground}>
-      <div className={styles.textOverlay}>
-        <h2 className="text-[2rem] font-bold mb-1">
-          TRUSTED DENVER TECH CENTER DENTISTS
-        </h2>
-        <h1 className="text-[3rem] font-bold mb-8">
-          Exceptional care for all ages
-        </h1>
-        <button className="bg-white text-black px-4 py-2 rounded-lg">
-          Book Now
-        </button>
+      <div className="container mx-auto border-4 border-white p-[20rem] rounded-br-[100px] rounded-tl-[100px]">
+        <div className={styles.textOverlay}>
+          <h2 className="text-[2rem] font-bold mb-1">
+            TRUSTED DENVER TECH CENTER DENTISTS
+          </h2>
+          <h1 className="text-[3rem] font-bold mb-8">
+            Exceptional care for all ages
+          </h1>
+          <button className="bg-white text-black px-4 py-2 rounded-lg">
+            Book Now
+          </button>
+        </div>
       </div>
     </div>
   );
