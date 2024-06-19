cbd1c4841d47bc3e0b40948c332eae7826b1057c
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2024-06-19 16:36:54 +0800

    fix red line

--- components/pages/Services.js
+++ components/pages/Services.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
